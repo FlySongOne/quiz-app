77f5166ff6841c3f0657a859a180b4d794aa7b97
Kyung Song <kyunghan.song@gmail.com>
2017-09-20 10:14:00 -0400

    routes, models, controllers

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,1 +1,70 @@
+//controller
+// import model
+const User = require('../models/users')
+//declare an empty usersController object
+const usersController = {};
+
+usersController.index = (req, res) => {
+  User.findAll()
+    .then(users=> {
+        res.render('users/users-index',{
+        message: 'ok',
+        data: users,
+      });
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}
+
+usersController.show = (req,res) => {
+   User.findById(req.params.id)
+   .then(users =>{
+      res.render('users/users-single',{
+         message:'ok',
+         data: users,
+      });
+   }).catch(err=>{
+      console.log(err);
+      res.status(500).json(err);
+   })
+};
+
+usersController.create = (req, res) => {
+   User.create({
+     username: req.body.username,
+     password: req.body.password,
+     name: req.body.name
+   }).then(users => {
+
+     res.redirect('/users');
+   }).catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   });
+};
+usersController.update = (req, res) => {
+   User.update({
+     username: req.body.username,
+     password: req.body.password,
+     name: req.body.name,
+   }, req.params.id).then(user => {
+     res.redirect('/users');
+   }).catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   })
+};
+
+usersController.delete = (req, res) => {
+   User.destroy(req.params.id)
+     .then(() => {
+       res.redirect('/user');
+     }).catch(err => {
+       console.log(err);
+      res.status(500).json(err);
+     });
+};
+
+module.exports = usersController;
 
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -4,7 +4,14 @@ const usersController = require('../controllers/usercontroller');
 const userRoutes = express.Router();
 
 userRoutes.get('/', usersController.index);
-userRoutes.get('/:id', usersController.show);
+
 userRoutes.post('/', usersController.create);
+
+userRoutes.get('/add', (req, res) => {
+   res.render('users/users-add');
+});
+userRoutes.put('/:id', usersController.update);
+userRoutes.get('/:id', usersController.show);
+userRoutes.delete('/:id', usersController.delete);
 
 module.exports = userRoutes;
