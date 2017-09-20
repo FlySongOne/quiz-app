596c1b90d6e7a30d096c798b085fc160f9bfcacb
nayeli cruz <nayelicruz12@gmail.com>
2017-09-20 10:24:00 -0400

    server js, installed cors

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
+// setting up express
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 
+// setting up port
+const PORT = process.env.PORT || 3001;
+app.liisten(PORT, function() {
+  console.log(`Listening on port ${PORT}`);
+});
+
+// setting up static file
+app.use('/static', express.static(path.join(__dirname, 'public')));
+// setting up cors
+app.use(cors());
+// setting up logger
+app.use(logger('dev'));
+// setting up body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// index route
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// API route
+const userRoutes = require('./routes/userroutes');
+app.use('/api/users', userRoutes);
+
+//throw in error 404
+app.get('*', function(req, res) {
+  res.status(404).send({message: 'Oops! Not found'});
+});
