b37d3479e62c2c144d265a7d3303969d84805f81
Kyung Song <kyunghan.song@gmail.com>
2017-09-20 15:30:31 -0400

    app.js

--- client/quizilla/src/App.js
+++ client/quizilla/src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor(){
+     super();
+     this.state = {
+         quizes : [],
+
+     }
+  }
+  componentDidMount(){
+    console.log("component didMount");
+     axios('https://api.quizlet.com/2.0/sets/415?access_token=wa8vnvhGhsebXTcpcT7BJbQTPrMDVB6UyHyeXU36&whitespace=1')
+         .then(res =>{
+             this.setState(prevState =>{
+                console.log(res.data);
+                return{
+                    quizes: res.data
+                }
+             });
+         });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +32,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2> {this.state.quizes} </h2>
       </div>
     );
   }
