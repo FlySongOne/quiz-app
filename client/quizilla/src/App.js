5a52cba480c071975bf534a89958b98768e0729c
Kyung Song <kyunghan.song@gmail.com>
2017-09-21 10:02:06 -0400

    update on app.js, about and home.jsx

--- client/quizilla/src/App.js
+++ client/quizilla/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Route, Redirect, Switch}from 'react-router-dom';
 
-
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   constructor(){
@@ -33,8 +37,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Header />
+        <main>
+          <Switch>
+
+           <Route exact path='/' component={Home}/>
+           <Route exact path='/about' component={About} />
+           <Redirect to='/' />
+
+          </Switch>
+        </main>
+
+        <Footer />
         <p>Here are your quizzes</p>
-          {this.state.quizes}
+
+
+
       </div>
     );
   }
---
+++ client/quizilla/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+       <p>About Us!</p>
+       <p>We are awesome!</p>
+    );
+  };
+}
+
+export default About;
