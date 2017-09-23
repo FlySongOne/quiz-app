734a60adce8b80641aaf142f3dd63da558b5ec0e
Yan Zhuk <yan.zhuk45@gmail.com>
2017-09-23 15:01:50 -0400

    resovle merge conflict

--- client/quizilla/src/App.js
+++ client/quizilla/src/App.js
@@ -3,46 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Route, Redirect, Switch}from 'react-router-dom';
+
 import Header from './components/partials/Header';
 import Footer from './components/partials/Footer';
 import Home from './components/Home';
 import About from './components/About';
+import QuestionList from './components/QuestionList';
+
 class App extends Component {
   constructor(){
      super();
      this.state = {
          quizes: [],
          questions: [],
+
      }
      this.score = 0;
-     this.counter = 0;
+     this.answerCounter = 0;
+     this.questionCounter = 1;
      this.limit = 0;
      this.handleClick = this.handleClick.bind(this);
+
   }
-//this.setState({
-//           question: res.data.data.questions[this.counter].question,
-//    })
+
+
   componentDidMount(){
     console.log("component didMount");
      axios('http://localhost:3001/api/questions/')
       .then(res => {
-    this.limit = res.data.data.questions.length-1;
+    this.limit = res.data.data.questions.length;
     this.setState({
            question: res.data.data.questions[0].question,
+
      })
     });
+
   }
+
   handleClick(){
     let input = document.getElementById('input').value
+
     console.log('Inside handleClick');
     this.questionFunc(input);
+    let field = document.getElementById('input');
+    field = "";
   }
+
   questionFunc(input){
+
     axios('http://localhost:3001/api/questions/')
       .then(res => {
         this.setState({
-           answer: res.data.data.questions[this.counter].answer,
-           question: res.data.data.questions[this.counter+1].question,
+           answer: res.data.data.questions[this.answerCounter].answer,
+           question: res.data.data.questions[this.questionCounter].question,
         })
         if(input === this.state.answer)
         {
@@ -54,35 +67,54 @@ class App extends Component {
         }else{
            console.log("Wrong answer!");
         }
+
         console.log("handle click", input, this.state.answer, this.score );
-        if( this.counter < this.limit-1){
-           this.counter += 1;
+
+        if( this.answerCounter < 5){
+           this.answerCounter += 1;
         }
+        if(this.questionCounter < 4){
+           this.questionCounter += 1;
+        }
+
+
     })
+
   }
+
+  yourScore(){
+     console.log('Your score is '+this.score+'/50');
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <Header />
+        </div>
+
         <main>
           <Switch>
            <Route exact path='/' component={Home}/>
+           <Route exact path='/quizzes' component={QuestionList} />
            <Route exact path='/about' component={About} />
            <Redirect to='/' />
           </Switch>
+
+
           <h3>{this.state.question}</h3>
-          <input id='input'></input>
+          <input id='input' placeholder='your answer'></input>
           <button onClick={this.handleClick}>Answer</button>
-          <h3>{this.state.score}</h3>
+          <h3>Your Score: {this.state.score} of 50</h3>
+
         </main>
+
         <Footer />
-        <p>Here are your quizzes</p>
+
+
       </div>
     );
   }
 }
+
 export default App;
