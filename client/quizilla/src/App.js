2be26f4b3514e78f95a4b98078bfddd5485bdccd
Yan Zhuk <yan.zhuk45@gmail.com>
2017-09-23 15:26:51 -0400

    add quiz component, move logic from App to Quiz

--- client/quizilla/src/App.js
+++ client/quizilla/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Route, Redirect, Switch}from 'react-router-dom';
-
+import Quiz from './components/Quiz';
 import Header from './components/partials/Header';
 import Footer from './components/partials/Footer';
 import Home from './components/Home';
@@ -11,80 +11,6 @@ import About from './components/About';
 import QuestionList from './components/QuestionList';
 
 class App extends Component {
-  constructor(){
-     super();
-     this.state = {
-         quizes: [],
-         questions: [],
-
-     }
-     this.score = 0;
-     this.answerCounter = 0;
-     this.questionCounter = 1;
-     this.limit = 0;
-     this.handleClick = this.handleClick.bind(this);
-
-  }
-
-
-  componentDidMount(){
-    console.log("component didMount");
-     axios('http://localhost:3001/api/questions/')
-      .then(res => {
-    this.limit = res.data.data.questions.length;
-    this.setState({
-           question: res.data.data.questions[0].question,
-
-     })
-    });
-
-  }
-
-  handleClick(){
-    let input = document.getElementById('input').value
-
-    console.log('Inside handleClick');
-    this.questionFunc(input);
-    let field = document.getElementById('input');
-    field = "";
-  }
-
-  questionFunc(input){
-
-    axios('http://localhost:3001/api/questions/')
-      .then(res => {
-        this.setState({
-           answer: res.data.data.questions[this.answerCounter].answer,
-           question: res.data.data.questions[this.questionCounter].question,
-        })
-        if(input === this.state.answer)
-        {
-           console.log("Correct answer!");
-           this.score += 10;
-           this.setState({
-              score: this.score,
-           })
-        }else{
-           console.log("Wrong answer!");
-        }
-
-        console.log("handle click", input, this.state.answer, this.score );
-
-        if( this.answerCounter < 5){
-           this.answerCounter += 1;
-        }
-        if(this.questionCounter < 4){
-           this.questionCounter += 1;
-        }
-
-
-    })
-
-  }
-
-  yourScore(){
-     console.log('Your score is '+this.score+'/50');
-  }
 
   render() {
     return (
@@ -92,26 +18,15 @@ class App extends Component {
         <div className="App-header">
         <Header />
         </div>
-
         <main>
           <Switch>
            <Route exact path='/' component={Home}/>
-           <Route exact path='/quizzes' component={QuestionList} />
+           <Route exact path='/quizzes' component={Quiz} />
            <Route exact path='/about' component={About} />
            <Redirect to='/' />
           </Switch>
-
-
-          <h3>{this.state.question}</h3>
-          <input id='input' placeholder='your answer'></input>
-          <button onClick={this.handleClick}>Answer</button>
-          <h3>Your Score: {this.state.score} of 50</h3>
-
         </main>
-
         <Footer />
-
-
       </div>
     );
   }
---
+++ client/quizilla/src/components/Quiz.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Header from './partials/Header';
+import Footer from './partials/Footer';
+import Home from './Home';
+import About from './About';
+import QuestionList from './QuestionList';
+
+class Quiz extends Component {
+  constructor(){
+   super();
+   this.state = {
+       quizes: [],
+       questions: [],
+
+   }
+   this.score = 0;
+   this.answerCounter = 0;
+   this.questionCounter = 1;
+   this.limit = 0;
+   this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount(){
+    console.log("component didMount");
+     axios('http://localhost:3001/api/questions/')
+      .then(res => {
+    this.limit = res.data.data.questions.length;
+    this.setState({
+           question: res.data.data.questions[0].question,
+
+     })
+    });
+  }
+handleClick(){
+    let input = document.getElementById('input').value
+
+    console.log('Inside handleClick');
+    this.questionFunc(input);
+    let field = document.getElementById('input');
+    field = "";
+  }
+
+  questionFunc(input){
+
+    axios('http://localhost:3001/api/questions/')
+      .then(res => {
+        this.setState({
+           answer: res.data.data.questions[this.answerCounter].answer,
+           question: res.data.data.questions[this.questionCounter].question,
+        })
+        if(input === this.state.answer)
+        {
+           console.log("Correct answer!");
+           this.score += 10;
+           this.setState({
+              score: this.score,
+           })
+        }else{
+           console.log("Wrong answer!");
+        }
+        console.log("handle click", input, this.state.answer, this.score )
+        if( this.answerCounter < 5){
+           this.answerCounter += 1;
+        }
+        if(this.questionCounter < 4){
+           this.questionCounter += 1;
+        }
+    })
+  }
+  yourScore(){
+     console.log('Your score is '+this.score+'/50');
+  }
+  render() {
+    return(
+      <div>
+        <h3>{this.state.question}</h3>
+        <input id='input' placeholder='your answer'></input>
+        <button onClick={this.handleClick}>Answer</button>
+        <h3>Your Score: {this.state.score} of 50</h3>
+      </div>
+      )
+  }
+
+}//Closing for Class Quiz
+
+export default Quiz;
