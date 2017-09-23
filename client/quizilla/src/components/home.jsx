a282bbdb4146c54dfc1db301dbf6ff402c24667c
Kyung Song <kyunghan.song@gmail.com>
2017-09-23 17:34:29 -0400

    form submit user database

--- client/quizilla/src/components/home.jsx
+++ client/quizilla/src/components/home.jsx
@@ -1,7 +1,50 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 class Home extends Component {
+  constructor(){
+     super();
+     this.state = {
+        users: [],
+        inputUsernameVal:'',
+        inputPasswordVal:'',
+     }
+     this.handleSubmit = this.handleSubmit.bind(this);
+   //  this.inputUsernameVal='';
+   //  this.inputPasswordVal='';
+  }
+
+  handleSubmit(event){
+    console.log('handleSubmit' , this.refs.inputUsernameVal);
+    event.preventDefault();
+    // event.target.username='';
+    // event.target.password='';
+
+     axios.post('http://localhost:3001/api/users/', {
+       username: this.state.inputUsernameVal,
+       password: this.state.inputPasswordVal,
+     })
+     .then(res => { console.log('after res', this)
+      {
+        const newUser ={
+
+           username: res.data.data.user.username,
+           password: res.data.data.user.password,
+        }
+        console.log("handleSub" , res);
+        this.setState((prevState)=>{
+          return{
+            users: prevState.users.concat(newUser)
+          }
+        })
+      }
+    }).catch(err =>console.log(err));
+  }
+
+
+
+
   render() {
     return (
 
@@ -9,15 +52,15 @@ class Home extends Component {
     <div id='form'>
       <p>Welcome to My Quizzilla!</p>
       <p>Please Log In</p>
-        <form className="SubmitForm" >
+        <form className="SubmitForm" onSubmit={this.handleSubmit}>
            <input
-           value= {this.props.username}
+           value= {this.refs.inputUsernameVal}
            className="nameInput"
            type="text"
            placeholder="User Name"
            />
            <input
-           value= {this.props.password}
+           value= {this.inputPasswordVal}
            className='nameInput'
            type='text'
            placeholder='Password'
@@ -25,7 +68,7 @@ class Home extends Component {
           <button
             type="submit"
             className="Button"
-            id="submit"><Link type="submit" to="/quizzes">Enter</Link>
+            id="submit">
           </button>
         </form>
       </div>
