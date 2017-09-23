8df818ca9b2e62c3d30b4d8df5f0b7a97a8214ba
Kyung Song <kyunghan.song@gmail.com>
2017-09-23 18:10:50 -0400

    update on Home.js and app.js

--- client/quizilla/src/components/home.jsx
+++ client/quizilla/src/components/home.jsx
@@ -3,47 +3,6 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 class Home extends Component {
-  constructor(){
-     super();
-     this.state = {
-        users: [],
-        inputUsernameVal:'',
-        inputPasswordVal:'',
-     }
-     this.handleSubmit = this.handleSubmit.bind(this);
-   //  this.inputUsernameVal='';
-   //  this.inputPasswordVal='';
-  }
-
-  handleSubmit(event){
-    console.log('handleSubmit' , this.refs.inputUsernameVal);
-    event.preventDefault();
-    // event.target.username='';
-    // event.target.password='';
-
-     axios.post('http://localhost:3001/api/users/', {
-       username: this.state.inputUsernameVal,
-       password: this.state.inputPasswordVal,
-     })
-     .then(res => { console.log('after res', this)
-      {
-        const newUser ={
-
-           username: res.data.data.user.username,
-           password: res.data.data.user.password,
-        }
-        console.log("handleSub" , res);
-        this.setState((prevState)=>{
-          return{
-            users: prevState.users.concat(newUser)
-          }
-        })
-      }
-    }).catch(err =>console.log(err));
-  }
-
-
-
 
   render() {
     return (
@@ -52,15 +11,15 @@ class Home extends Component {
     <div id='form'>
       <p>Welcome to My Quizzilla!</p>
       <p>Please Log In</p>
-        <form className="SubmitForm" onSubmit={this.handleSubmit}>
+        <form className="SubmitForm" onSubmit={this.props.handleSubmit}>
            <input
-           value= {this.refs.inputUsernameVal}
+           value= {this.props.inputUsernameVal}
            className="nameInput"
            type="text"
            placeholder="User Name"
            />
            <input
-           value= {this.inputPasswordVal}
+           value= {this.props.inputPasswordVal}
            className='nameInput'
            type='text'
            placeholder='Password'
