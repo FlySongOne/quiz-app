6ef4da098cb0611b870c3dd360544bd8259e8a82
Yan Zhuk <yan.zhuk45@gmail.com>
2017-09-25 18:03:21 -0400

    add overlay css

---
+++ client/quizilla/src/components/GameSummary.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class GameSummary extends Component {
+
+ render() {
+    return (
+    <div id='overlay'>
+      <p id='text'>game summary{this.props.numberCorrect}</p>
+    </div>
+
+    );
+  };
+}
+
+export default GameSummary;
