3e367812ee3008d43c849a2a6a827e1d43f3b5fb
Yan Zhuk <yan.zhuk45@gmail.com>
2017-09-26 01:33:47 -0400

    fix merge conflits and add css/overlay

--- client/quizilla/src/components/GameSummary.jsx
+++ client/quizilla/src/components/GameSummary.jsx
@@ -3,13 +3,15 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 class GameSummary extends Component {
-
  render() {
+  console.log("THIS PROPSSSSSSS", this.props)
     return (
-
-    <div id='summary'>
-      <p>game summary</p>
-        {this.props.numberCorrect}
+    <div id='overlay' style={{ display: this.props.displayVal}}>
+      <div id='text'>
+          <h1>game summary</h1>
+          <p>Number correct: {this.props.numberCorrect} </p>
+          <button onClick={this.props.changeDisplayVal}>OK</button>
+      </div>
     </div>
 
     );
