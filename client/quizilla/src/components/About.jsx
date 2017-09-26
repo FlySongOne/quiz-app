d0025730fd4e89fc1c2934ffa96b928240e05ccf
Yan Zhuk <yan.zhuk45@gmail.com>
2017-09-26 00:59:36 -0400

    add song file changes

--- client/quizilla/src/components/About.jsx
+++ client/quizilla/src/components/About.jsx
@@ -4,9 +4,16 @@ class About extends Component {
   render() {
     return (
       <div>
-       <p>About Us!</p>
+       <h3>About Us!</h3>
+       <p>  It's the 3 best friends that anybody could have.</p>
+       <h3>About Quizilla App</h3>
        <article>
-       It's the 3 best friends that anybody could have
+         <p>As a user I want see the login form when I visit home page.</p>
+         <p>I want to be able to enter my username and password when I press the login button.</p>
+         <p>As a logged in user, I want my information to persist between sessions.</p>
+         <p>I want to see the quiz questions one a time.</p>
+         <p>I want to see a score output</p>
+         <p>The game summary is displayed</p>
        </article>
       </div>
     );
